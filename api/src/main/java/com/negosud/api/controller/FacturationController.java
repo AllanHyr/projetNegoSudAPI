a4b2d35c2ed34240636c7f3f94c892f95d9538fe
Illona02 <illona.hardy@gmail.com>
2024-02-27 11:05:44 +0100

    change url

--- api/src/main/java/com/negosud/api/controller/FacturationController.java
+++ api/src/main/java/com/negosud/api/controller/FacturationController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Optional;
 
 @RestController
-@CrossOrigin("http://localhost:9000/")
+@CrossOrigin("http://localhost:9300/")
 public class FacturationController {
 
     @Autowired
